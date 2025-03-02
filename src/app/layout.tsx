9a37bf0fe763550add1d44cd84a8c1306960c5fa
twizzworld <103385201+numinousmuses@users.noreply.github.com>
2025-03-01 23:11:16 -0600

    added cxmpute brand colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Roboto_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Roboto_Mono, Roboto } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -17,6 +17,11 @@ const robotoMono = Roboto_Mono({
   subsets: ["latin"],
 });
 
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
   title: "Cxmpute - The World Datacenter",
   description: "Redefining the utility of idle hardware.",
@@ -29,7 +34,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${robotoMono.variable} ${roboto.variable}`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,35 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from "@/components/button/button";
+
+const cxmputeGreen = "#20a191";
+const cxmputePink = "#fe91e8";
+const cxmputeYellow = "f8cb46";
+const cxmputePurple = "#91a8eb";
+const cxmputeRed = "#d64989";
+const cxmputeSand = "#d4d4cb";
+const cxmputeSlate = "#d4d4cb";
+const cxmputeBeige = "#f9f5f2";
+const cxmputeBeigerBeige = "#fdede3";
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <header className={styles.header}>
         <div className={styles.logo}>
-          <Image src="/dolphinhero.png" alt="cxmpute logo" height={70} width={70}/>
+          <Image src="/1.png" alt="cxmpute logo" height={70} width={70}/>
+          <h1>CXMPUTE</h1>
         </div>
-
+        <div className={styles.menu}>
+          <ul>
+            <li><a href="/download">
+              <Button text="DOWNLOAD" backgroundColor="#f0f0f0" />
+            </a></li>
+            <li><a href="/docs">DOCUMENTATION</a></li>
+            <li><a href="/dashboard">OPEN DASHBOARD</a></li>
+          </ul>
+        </div>
       </header>
       {/* Who is cxmpute for */}
       <div className={styles.content}></div>
