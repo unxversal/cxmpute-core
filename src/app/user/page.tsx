7efddf30cca20aac56449d644834c2fc5010040d
twizzworld <103385201+numinousmuses@users.noreply.github.com>
2025-05-18 22:24:21 -0500

    Removed cache

--- src/app/user/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './user.module.css';
-
-export default function UserPage() {
-    return (
-        <main className={styles.main}>
-            <h1>User Page</h1>
-        </main>
-    );
-}
--- src/lib/genSchemaCad.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const GenResultCad = z.object({
-  code: z.string(),
-  commentary: z.string().optional(),
-  finished: z.boolean().default(false),
-});
-
-export type GenResultCadType = z.infer<typeof GenResultCad>;
